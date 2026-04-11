987bd1281b922871ac00a639c4c7f0c4d6fc1c66
Pantelis <pantelos1999@gmail.com>
2026-04-11 17:08:58 +0300

    typewritter effect and get resume features

--- src/app/introduction/introduction.component.ts
+++ src/app/introduction/introduction.component.ts
@@ -8,5 +8,50 @@ import { Component } from '@angular/core';
   styleUrl: './introduction.component.css'
 })
 export class IntroductionComponent {
+  jobTitles: string[] = ["Full Stack Developer", "Software Engineer","Frontend Developer", "Backend Developer", "Data Analyst", "Problem Solver" ]
 
+  typewrittervisibility:boolean = true;
+displayedTitle = "";
+cursorVisible = true;
+
+private jobIndex = 0;
+private charIndex = 0;
+
+ngOnInit() {
+  this.startTyping();
+  this.blinkCursor();
 }
+
+startTyping() {
+  const typingSpeed = 80;
+  const pauseTime = 1200;
+
+  const typeTitle = () => {
+    const currentWord = this.jobTitles[this.jobIndex];
+
+    if (this.charIndex < currentWord.length) {
+      this.charIndex++;
+      this.displayedTitle = currentWord.substring(0, this.charIndex);
+
+      setTimeout(typeTitle, typingSpeed);
+    } else {
+      // pause when word is fully typed
+      setTimeout(() => {
+        this.jobIndex = (this.jobIndex + 1) % this.jobTitles.length;
+        this.charIndex = 0;
+        this.displayedTitle = "";
+        typeTitle();
+      }, pauseTime);
+    }
+  };
+
+  typeTitle();
+}
+
+blinkCursor() {
+  setInterval(() => {
+    this.cursorVisible = !this.cursorVisible;
+  }, 500);
+}
+}
+
